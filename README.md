8ad1f7fb58eb02df36fdeec4505eb1e9e0d3643f
Ayush GR <ayush.ramkumar@loom.com>
2025-11-23 22:05:28 +0530

    initial p10k setup

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # dotfiles
 My Dotfiles
 
-1. brew install chezmoi
+1. ./osx_init.sh
 2. chezmoi init https://github.com/ayushgr/dotfiles.git
 3. ./repo_init.sh
 4. chezmoi apply
---
+++ osx_init.sh
@@ -0,0 +1,15 @@
+# Install homebrew
+# ...
+
+# Install chezmoi
+brew install chezmoi
+
+# run chezmoi
+
+# Install terminal
+brew install --cask iterm2
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/itermconfigs"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Install zsh items
+brew install antidote
