f8ee77ee98520679ce1c60bf3ec5eb546d24ca14
Ayush GR <ayush.ramkumar@gmail.com>
2025-11-28 01:42:56 -0500

    add zsh abbr, bat, eza

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # dotfiles
-My Dotfiles
 
+My Dotfiles.
+
+## Setup Instructions
+1. Install Homebrew
+1. brew install chezmoi
+1. chezmoi init https://github.com/ayushgr/dotfiles.git
+1. chezmoi cd
+1. ./repo_init.sh
 1. ./osx_init.sh
-2. chezmoi init https://github.com/ayushgr/dotfiles.git
-3. ./repo_init.sh
-4. chezmoi apply
+1. chezmoi apply
--- osx_init.sh
+++ osx_init.sh
@@ -1,17 +1,17 @@
-# Install homebrew
-# ...
+# Install homebrew - Assuming installed outside of script
 
-# Install chezmoi
-brew install chezmoi
+# Install chezmoi - Assuming installed outside of script
+# brew install chezmoi
 
-# run chezmoi
+# Fonts
+brew install font-fira-mono-nerd-font
 
 # Install devtools
-brew install hl
-brew install rg
-brew install jq
-
-# install firemono nerd font for terminal
+brew install eza # Improved ls
+brew install bat # Improved cat
+brew install hl # Log parsing and viewing
+brew install rg # ripgrep
+brew install jq # json parsing
 
 # Install terminal
 brew install --cask iterm2
@@ -20,5 +20,31 @@ defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
-# Install zsh items
+# Install zsh and terminal items
 brew install antidote
+
+# Consider new Tools -> https://github.com/johnalanwoods/maintained-modern-unix
+# Consider atuin https://atuin.sh/
+# Consider bottom https://formulae.brew.sh/formula/bottom
+# Consider difftastic
+# zoxide - modern cd
+# fd - find alternative
+# rg - ripgrep (grep)
+# fzf - fuzzy finder
+# jless - json viewer with mouse folding
+# dra - download and install release assets
+# lazygit - git TUI
+# lazydocker - docker TUI
+# yazi - file manager with image preview
+# zellij - better tmux / screen
+
+# Install Applications
+brew install --cask claude-code
+brew install amphetamine
+# Todo Alt-Tab + Config
+# Todo Rectangle + Config
+# Todo Raycast + Config
+# Todo Flux
+# Todo Dropbox
+# Todo Superwhisper
+# Todo Magic Switch
